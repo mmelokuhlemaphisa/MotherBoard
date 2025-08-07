c0ec00674f5e16850a2c4ccce55e1886e35b63a7
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-08-07 09:09:12 +0200

    Done with  motherboard page

--- script.js
+++
@@ -1,6 +0,0 @@
-const hiddenText = document.getElementById('hidden-Text');
-const showPopupButton = document.getElementById('Show-popup-Button');
-
-  showPopupButton.addEventListener('click', () => {
-    hiddenText.classList.toggle('hidden') 
-  })
